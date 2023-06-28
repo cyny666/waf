0ee74b69a77ff3d3e5afef698f2a8aba515adb48
cyny666 <1468578931@qq.com>
2023-06-28 13:52:17 +0800

    basic finished

--- mod_firewall/mod_firewall/configure.c
+++ mod_firewall/mod_firewall/configure.c
@@ -4,31 +4,60 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
-#include	<stdlib.h>
+#include <stdlib.h>
+#include <time.h>
 
-unsigned int controlled_protocol = 0;
-unsigned short controlled_srcport = 0;
-unsigned short controlled_dstport = 0;
-unsigned int controlled_saddr = 0;
-unsigned int controlled_daddr = 0; 
-// 展示程序的使用说明
-void display_usage(char *commandname)
+#define MAX_ICMP_TYPES 10
+
+unsigned int controlled_protocol = 0;	//协议名
+unsigned short controlled_srcport = 0;	//源端口号
+unsigned short controlled_dstport = 0;	//目的端口号
+unsigned int controlled_saddr = 0;	//源IP地址
+unsigned int controlled_daddr = 0; 	//目的IP地址
+
+//时间控制信息
+int controlled_time_flag=0; //0表示时间判断功能关闭
+unsigned int  controlled_time_begin=0;
+unsigned int controlled_time_end=0;
+int icmp_type[9]={0,0,0,0,0,0,0,0,0};
+
+//网络接口
+int controlled_interface=0;
+
+// 回显应答报文（Echo Reply）：宏定义 ICMP_ECHOREPLY，值为 0。
+// 回显请求报文（Echo Request）：宏定义 ICMP_ECHO，值为 8。
+// 目标不可达报文（Destination Unreachable）：宏定义 ICMP_DEST_UNREACH，值为 3。
+// 超时报文（Time Exceeded）：宏定义 ICMP_TIME_EXCEEDED，值为 11。
+// 参数问题报文（Parameter Problem）：宏定义 ICMP_PARAMETERPROB，值为 12。
+// 源抑制报文（Source Quench）：宏定义 ICMP_SOURCE_QUENCH，值为 4。
+// 重定向报文（Redirect）：宏定义 ICMP_REDIRECT，值为 5。
+// 时间戳请求报文（Timestamp Request）：宏定义 ICMP_TIMESTAMP，值为 13。
+// 时间戳应答报文（Timestamp Reply）：宏定义 ICMP_TIMESTAMPREPLY，值为 14。
+
+
+
+
+void display_usage(char *commandname)	//输出显示命令用法
 {
 	printf("Usage 1: %s \n", commandname);
-	printf("Usage 2: %s -x saddr -y daddr -m srcport -n dstport \n", commandname);
+	printf("Usage 2: %s -x saddr -y daddr -m srcport -n dstport -t time_control \n", commandname);
 }
-// 解析命令行参数并获取相应的参数值
+
+//命令行参数解析的函数
 int getpara(int argc, char *argv[]){
-	int optret;
-	unsigned short tmpport;
-    // argc代表命令行参数的数量、argv代表命令函参数的值、pxymnh代表指定的选项
-	optret = getopt(argc,argv,"pxymnh");
-	while( optret != -1 ) {
+	int optret;	//
+	unsigned short tmpport;	//存储临时端口号
+
+ 	char *icmp_types[MAX_ICMP_TYPES];
+    int num_icmp_types = 0;
+
+
+	optret = getopt(argc,argv,"pxymnhbeti");
+	while( optret != -1 ) {		
 //			printf(" first in getpara: %s\n",argv[optind]);
         	switch( optret ) {
-            // 处理源端口、确定使用的协议类型 controlled_protocol用来识别哪一个协议
-        	case 'p':
-        		if (strncmp(argv[optind], "ping",4) == 0 )
+        	case 'p':	//-p protocol 指明要控制的协议（或网络应用）类型，具体为 tcp、udp、icmp三种之一；
+        		if (strncmp(argv[optind], "icmp",4) == 0 )
 					controlled_protocol = 1;
 				else if ( strncmp(argv[optind], "tcp",3) == 0  )
 					controlled_protocol = 6;
@@ -39,21 +68,19 @@ int getpara(int argc, char *argv[]){
 					exit(1);
 				}
         		break;
-         case 'x':   //获取命令行参数中指定的源 IP 地址，并将其转换为可用于防火墙规则中的格式
-                     // 以便在防火墙规则中设置针对该源 IP 地址的控制策略。
-                     // struct in_addr是结构体
+         case 'x':   //get source ipaddr， –x source_ip 指明要控制报文的源IP地址；
 				if ( inet_aton(argv[optind], (struct in_addr* )&controlled_saddr) == 0){
 					printf("Invalid source ip address! please check and try again! \n ");
 					exit(1);
 				}
          	break;
-         case 'y':   //获取目标的的ip地址
+         case 'y':   //get destination ipaddr，–y dst_ip 指明要控制报文的目标IP地址；
 				if ( inet_aton(argv[optind], (struct in_addr* )&controlled_daddr) == 0){
 					printf("Invalid destination ip address! please check and try again! \n ");
 					exit(1);
 				}
          	break;
-         case 'm':   //获取目标的端口
+         case 'm':   //get destination ipaddr， –m source_port 指明要控制报文的源端口地址；
 				tmpport = atoi(argv[optind]);
 				if (tmpport == 0){
 					printf("Invalid source port! please check and try again! \n ");
@@ -61,7 +88,7 @@ int getpara(int argc, char *argv[]){
 				}
 				controlled_srcport = htons(tmpport);
          	break;
-        case 'n':   //获取源端口
+        case 'n':   //get destination ipaddr，–n dst_port 指明要控制报文的目标端口；
 				tmpport = atoi(argv[optind]);
 				if (tmpport == 0){
 					printf("Invalid source port! please check and try again! \n ");
@@ -69,30 +96,75 @@ int getpara(int argc, char *argv[]){
 				}
 				controlled_dstport = htons(tmpport);
          	break;
-         case 'h':   /* fall-through is intentional */
-         case '?':
+
+        case 'b':
+				const char *time_str_begin=argv[optind];
+				struct tm time_info_begin;
+				// 解析时间字符串
+				if (strptime(time_str_begin, "%H:%M", &time_info_begin) == NULL) {
+					printf("开始时间解析失败\n");
+					exit(1);
+				}
+                controlled_time_flag=1;
+                controlled_time_begin=time_info_begin.tm_hour*60+time_info_begin.tm_min;
+                break;
+        case 'e':
+				const char *time_str_end=argv[optind];
+				struct tm time_info_end;
+				// 解析时间字符串
+				if (strptime(time_str_end, "%H:%M", &time_info_end) == NULL) {
+					printf("结束时间解析失败\n");
+					exit(1);
+				}
+                controlled_time_flag=1;
+                controlled_time_end=time_info_end.tm_hour*60+time_info_end.tm_min;
+                break;
+		case 't':
+				
+				int tmp_icmp_type = atoi(argv[optind]);
+                if (tmp_icmp_type == 0){
+					printf("Invalid icmp type! please check and try again! \n ");
+					exit(1);
+				}
+				icmp_type[tmp_icmp_type-1]=1;
+                break;
+        case 'i':
+                int tmp_interface=atoi(argv[optind]);
+                if(tmp_interface==0){
+                    printf("Invalid network interface! please check and try again! \n ");
+					exit(1);
+                }
+                controlled_interface=tmp_interface;
+                break;
+
+
+         case 'h':   /* fall-through is intentional ，故意失败*/ 
+         case '?':	//当解析到未知选项或无效选项时执行的操作。
+		 				//在这种情况下，通常会显示用法信息，并通过调用 display_usage(argv[0]) 函数来打印程序的使用说明。
+						//然后，使用 exit(1) 终止程序的执行，并返回退出状态码 1，
          	display_usage(argv[0]);
-         	exit(1);;
+         	exit(1);
                 
          default:
 				printf("Invalid parameters! \n ");
          	display_usage(argv[0]);
-         	exit(1);;
+         	exit(1);
         	}
-        // getopt每次调用会返回下一个参数
-		optret = getopt(argc,argv,"pxymnh");
+		optret = getopt(argc,argv,"pxymnhbeti");
 	}
+
+
 }
 
 int main(int argc, char *argv[]){
-	char controlinfo[32];
+	char controlinfo[128];
 	int controlinfo_len = 0;
 	int fd;
-	struct stat buf;
-	// argc代表选项的数量
-    // controlinfo_len代表控制信息的长度
+	struct stat buf;	//stat 结构体是用来存储文件或文件系统对象的状态信息的，例如文件的大小、访问权限、修改时间等。
+								//通过使用 struct stat buf，可以在程序中获取和操作文件的各种属性和元数据。
+	
 	if (argc == 1) 
-		controlinfo_len = 0; //取消防火墙
+		controlinfo_len = 0; //cancel the filter
 	else if (argc > 1){
 		getpara(argc, argv);
 		*(int *)controlinfo = controlled_protocol;
@@ -100,19 +172,29 @@ int main(int argc, char *argv[]){
 		*(int *)(controlinfo + 8) = controlled_daddr;
 		*(int *)(controlinfo + 12) = controlled_srcport;
 		*(int *)(controlinfo + 16) = controlled_dstport;
-		controlinfo_len = 20;
+        *(int *)(controlinfo + 20) = controlled_time_flag;
+        *(int *)(controlinfo + 24) = controlled_time_begin;
+        *(int *)(controlinfo + 28) = controlled_time_end;
+        for(int i=0;i<9;++i){
+			*(int *)(controlinfo + 32 + 4*i) = icmp_type[i];
+		}
+        *(int *)(controlinfo + 68) = controlled_interface;
+		controlinfo_len =74;
 	}
 	
 //	printf("input info: p = %d, x = %d y = %d m = %d n = %d \n", controlled_protocol,controlled_saddr,controlled_daddr,controlled_srcport,controlled_dstport);
- //查询/dev/controlinfo 的状态,若无则创建
- //mknod创建设备字符文件
-	if (stat("/dev/controlinfo",&buf) != 0){
-		if (system("mknod /dev/controlinfo c 124 0") == -1){
+
+	if (stat("/dev/controlinfo",&buf) != 0){	//这段代码是在检查文件/dev/controlinfo的状态。stat 函数用于获取文件的状态信息，并将结果存储在提供的结构体变量 buf 中。
+		/*这段代码是在尝试通过系统命令mknod创建一个名为/dev/controlinfo的字符设备文件。
+		mknod命令用于创建设备文件，其中c表示创建字符设备文件，124表示设备文件的主设备号，0表示设备文件的次设备号。*/
+		if (system("mknod /dev/controlinfo c 124 0") == -1){	
 			printf("Cann't create the devive file ! \n");
 			printf("Please check and try again! \n");
 			exit(1);
 		}
 	}
+
+	/*将字符串controlinfo的命令信息写入到字符设备文件"/dev/controlinfo"中*/
 	fd =open("/dev/controlinfo",O_RDWR,S_IRUSR|S_IWUSR);
 	if (fd > 0)
 	{
@@ -124,3 +206,4 @@ int main(int argc, char *argv[]){
 	}
 	close(fd);
 }
+
--- mod_firewall/mod_firewall/mod_firewall.c
+++ mod_firewall/mod_firewall/mod_firewall.c
@@ -12,22 +12,80 @@
 #include <linux/icmp.h>
 #include <linux/udp.h>
 
-#define MATCH	1 // 表示符合要求
-#define NMATCH	0  //not match
+
+#include <linux/ktime.h>
+#include <linux/time.h>
+#include <linux/time64.h>
+
+
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/netdevice.h>
+#include <linux/etherdevice.h>
+
+#define MATCH		1
+#define NMATCH	0
 
 int enable_flag = 0;
 
-struct nf_hook_ops myhook;
+struct nf_hook_ops myhook;	//Netfilter 框架中定义的结构体，用于注册和管理钩子函数。它包含了一些字段和回调函数，用于指定钩子函数的行为和属性。
 
+/**保存真正使用的规则信息*/
 unsigned int controlled_protocol = 0;
 unsigned short controlled_srcport = 0;
 unsigned short controlled_dstport = 0;
 unsigned int controlled_saddr = 0;
 unsigned int controlled_daddr = 0;
 
+//时间控制信息
+int controlled_time_flag=0; //0表示时间判断功能关闭
+unsigned int  controlled_time_begin=0;  
+unsigned int controlled_time_end=0;  
+
+//网络接口
+int controlled_interface=0;
+
+//icmp报文类型
+int icmp_type[9]={0,0,0,0,0,0,0,0,0};
+/*
+0: 回显应答报文（Echo Reply）：宏定义 ICMP_ECHOREPLY，值为 0。
+1: 回显请求报文（Echo Request）：宏定义 ICMP_ECHO，值为 8。
+2: 目标不可达报文（Destination Unreachable）：宏定义 ICMP_DEST_UNREACH，值为 3。
+3: 超时报文（Time Exceeded）：宏定义 ICMP_TIME_EXCEEDED，值为 11。
+4: 参数问题报文（Parameter Problem）：宏定义 ICMP_PARAMETERPROB，值为 12。
+5: 源抑制报文（Source Quench）：宏定义 ICMP_SOURCE_QUENCH，值为 4。
+6: 重定向报文（Redirect）：宏定义 ICMP_REDIRECT，值为 5。
+7: 时间戳请求报文（Timestamp Request）：宏定义 ICMP_TIMESTAMP，值为 13。
+8: 时间戳应答报文（Timestamp Reply）：宏定义 ICMP_TIMESTAMPREPLY，值为 14。
+*/
+struct KeyValuePair {
+    int key;
+    int value;
+};
+struct KV_interface{
+	int key;
+	char value[10];
+};
+struct KeyValuePair icmp_type_reflection[9]={
+    {0,0},
+    {1,8},
+    {2,3},
+    {3,11},
+    {4,12},
+    {5,4},
+    {6,5},
+    {7,13},
+    {8,14}
+};
+struct KV_interface interface_available[2]={
+	{1,"ens33"},
+	{2,"lo"}
+};
 struct sk_buff *tmpskb;
 struct iphdr *piphdr;
-// 检查port是否正确
+
+/*检查端口号是否匹配*/
 int port_check(unsigned short srcport, unsigned short dstport){
 	if ((controlled_srcport == 0 ) && ( controlled_dstport == 0 ))
 		return MATCH;
@@ -55,9 +113,9 @@ int port_check(unsigned short srcport, unsigned short dstport){
 	return NMATCH;
 }
 
-// 检查ip是否正确
+/*检测IP地址是否匹配，若匹配返回MATCH并丢弃网络包*/
 int ipaddr_check(unsigned int saddr, unsigned int daddr){
-	if ((controlled_saddr == 0 ) && ( controlled_daddr == 0 ))
+	if ((controlled_saddr == 0 ) && ( controlled_daddr == 0 ))	//未设置IP地址
 		return MATCH;
 	if ((controlled_saddr != 0 ) && ( controlled_daddr == 0 ))
 	{
@@ -82,30 +140,27 @@ int ipaddr_check(unsigned int saddr, unsigned int daddr){
 	}
 	return NMATCH;
 }
-// 过滤icmp（ping）协议
+
+// ICMP协议检查函数
 int icmp_check(void){
-	struct icmphdr *picmphdr;
+	struct icmphdr *picmphdr;	//struct icmphdr 是定义在 <linux/icmp.h> 头文件中的结构体，用于表示 ICMP（Internet Control Message Protocol）报文头部的信息。
 //  	printk("<0>This is an ICMP packet.\n");
-// picmphdr 变量就指向了 ICMP 报文头部的位置
-   picmphdr = (struct icmphdr *)(tmpskb->data +(piphdr->ihl*4));
-// picmphdr->type 是否等于 0，如果是，则表示这是一个回显请求（ICMP Echo Request）报文，需要进行进一步处理。
-	if (picmphdr->type == 0){
-			if (ipaddr_check(piphdr->daddr,piphdr->saddr) == MATCH){
-			 	printk("An ICMP packet is denied! \n");
-				return NF_DROP;
-                //如果匹配，表示此 ICMP 报文需要被禁止，代码使用 printk() 函数输出一条日志信息，并返回 NF_DROP，表示将数据包丢弃。
-			}
-	}
-    //如果 ICMP 报文的类型字段等于 8，表示这是一个回显应答（ICMP Echo Reply）报文
-	if (picmphdr->type == 8){
-			if (ipaddr_check(piphdr->saddr,piphdr->daddr) == MATCH){
+   picmphdr = (struct icmphdr *)(tmpskb->data +(piphdr->ihl*4));	//将该指针指向 ICMP 报文数据的位置
+	//printk("icmptype: %d",picmphdr->type);
+	for(int i=0;i<9;i++){
+	//printk("icmp[%d]: %d",i,icmp_type[i]);
+        if(picmphdr->type==(icmp_type_reflection[i]).value &&icmp_type[(icmp_type_reflection[i]).key]==1 ){
+            if (ipaddr_check(piphdr->daddr,piphdr->saddr) == MATCH){
 			 	printk("An ICMP packet is denied! \n");
 				return NF_DROP;
 			}
-	}
+        }
+    
+    }
     return NF_ACCEPT;
 }
-//过滤tcp协议
+
+
 int tcp_check(void){
 	struct tcphdr *ptcphdr;
 //   printk("<0>This is an tcp packet.\n");
@@ -117,7 +172,7 @@ int tcp_check(void){
 	else
       return NF_ACCEPT;
 }
-//过滤udp协议
+
 int udp_check(void){
 	struct udphdr *pudphdr;
 //   printk("<0>This is an udp packet.\n");
@@ -130,26 +185,109 @@ int udp_check(void){
       return NF_ACCEPT;
 }
 
+//返回0表示时间判断功能关闭 或当前时间符合规则
+int time_check(struct tm *tm){
+    if(controlled_time_flag==0) return 0;
+    else if(controlled_time_flag==1){
+        int current_mins=(8+tm->tm_hour)*60+tm->tm_min;
+
+        if(current_mins>controlled_time_begin && current_mins<controlled_time_end){
+			printk("Access is denied at this time!\n");
+            printk("current time: %d\n",current_mins);
+            printk("control time: %d ~ %d\n",controlled_time_begin,controlled_time_end);
+			return 1;
+		}
+        else return 0;
+    }
+    else return 0;
+}
+
+
+int net_interface_check(struct sk_buff *skb,const struct nf_hook_state *state){
+
+	if(controlled_interface==0)return 0;	//对接口无要求直接返回
+
+	struct net_device *dev_crl;
+    unsigned char *controlled_mac_addr;
+	
+	//获取网络包的MAC
+    struct net_device *dev = skb->dev;
+    unsigned char *mac_addr = dev->dev_addr;
+
+	// 获取指定的网络接口
+	const char* interface_con =(interface_available[controlled_interface-1]).value;
+    dev_crl = dev_get_by_name(&init_net, interface_con);  
+
+	
+
+    // 打印MAC地址
+    printk("packet MAC Address: %02x:%02x:%02x:%02x:%02x:%02x\n",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+
+
+
+    if (dev_crl) {
+        // 获取 MAC 地址的指针
+        controlled_mac_addr = dev_crl->dev_addr;
+
+        // 打印 MAC 地址
+        printk("controlled MAC address：%pM\n", controlled_mac_addr);
+        dev_put(dev_crl);
+    } else {
+        printk("Unable to get network card device!\n");
+    }
+
+
+	int result = memcmp(mac_addr, controlled_mac_addr, 6);
+	if(result==0)return 0;
+	else return 1;
+
+
+}
+
 /*unsigned int hook_func(unsigned int hooknum,struct sk_buff **skb,const struct net_device *in,const struct net_device *out,int (*okfn)(struct sk_buff *))
 */
+/*网络钩子函数 hook_func，用于网络数据包过滤和处理*/
 unsigned int hook_func(void * priv,struct sk_buff *skb,const struct nf_hook_state * state){
-//如果enable_flag=0表示防火墙被禁用
-	if (enable_flag == 0)
+	
+	
+	if (enable_flag == 0)	//网络过滤器被禁用，返回NF_ACCEPT表示接受该数据包
 		return NF_ACCEPT;
-    //tmpskb用于存储当前处理的数据包
-    //piphdr 变量用于存储数据包的 IP 报文头部
-   	tmpskb = skb;
-	piphdr = ip_hdr(tmpskb);
- //协议类型
+   	tmpskb = skb;	//将数据包传给全局变量
+	piphdr = ip_hdr(tmpskb);	//获取IP报文头部的指针
+	
+	//检查网络接口
+	if(net_interface_check(skb,state)==1){
+		printk("The current network interface is disabled!\n");
+		return NF_DROP;
+	}
+    //获取时间
+    struct timespec64 ts;
+    struct tm time_info;
+    ktime_get_real_ts64(&ts);  // 获取当前系统时间
+    time64_to_tm(ts.tv_sec, 0, &time_info);  // 将时间转换为本地时间
+
+    if(time_check(&time_info)==1)return NF_DROP;    //判断时间是否符合规则
+
 	if(piphdr->protocol != controlled_protocol)
       		return NF_ACCEPT;
-
+	
+	
 	if (piphdr->protocol  == 1)  //ICMP packet
+	{
+		printk("icmp\n");
 		return icmp_check();
+	}
 	else if (piphdr->protocol  == 6) //TCP packet
+	{
+		printk("tcp\n");
 		return tcp_check();
+	}
 	else if (piphdr->protocol  == 17) //UDP packet
+	{
+		printk("udp\n");
 		return udp_check();
+	}
 	else
 	{
 		printk("Unkonwn type's packet! \n");
@@ -169,6 +307,8 @@ static ssize_t write_controlinfo(struct file * fd, const char __user *buf, size_
 		return len;
 	}
 
+	/*函数调用了 copy_from_user 函数，它用于将用户空间的数据复制到内核空间的缓冲区中。
+	参数 controlinfo 是目标缓冲区的地址，buf 是源数据的地址，len 是要复制的数据长度。*/
 	if (copy_from_user(controlinfo, buf, len) != 0){
 		printk("Can't get the control rule! \n");
 		printk("Something may be wrong, please check it! \n");
@@ -183,44 +323,71 @@ static ssize_t write_controlinfo(struct file * fd, const char __user *buf, size_
 	controlled_srcport = *(( int *) pchar);
 	pchar = pchar + 4;
 	controlled_dstport = *(( int *) pchar);
+    pchar = pchar + 4;
+    controlled_time_flag = *(( int *) pchar);
+    pchar = pchar + 4;
+    controlled_time_begin = *(( int *) pchar);
+    pchar = pchar + 4;
+    controlled_time_end = *(( int *) pchar);
+    for(int i=0;i<9;i++){
+        pchar = pchar + 4;
+        icmp_type[i]=*(( int *) pchar);
+    }
+    pchar = pchar + 4;
+    controlled_interface=*(( int *) pchar);
+
 
 	enable_flag = 1;
-	printk("input info: p = %d, x = %d y = %d m = %d n = %d \n", controlled_protocol,controlled_saddr,controlled_daddr,controlled_srcport,controlled_dstport);
+	printk("input info: p = %d, x = %d y = %d m = %d n = %d; Time: flag = %d and %d ~ %d",
+     controlled_protocol,controlled_saddr,controlled_daddr,controlled_srcport,controlled_dstport,
+     controlled_time_flag,controlled_time_begin,controlled_time_end);
+    printk("rejected icmp type: ");
+    for(int i=0;i<9;i++){
+        if(icmp_type[i]==1){
+            printk(" %d",i+1);
+        }
+    }
+    printk("controlled interface %d",controlled_interface);
+    
 	return len;
 }
 
-
+//struct file_operations 是 Linux 内核中定义的结构体，用于描述设备文件的操作函数集合。通过初始化该结构体的成员，可以指定设备文件的操作函数。
 struct file_operations fops = {
-	.owner=THIS_MODULE,
-	.write=write_controlinfo,
+	.owner=THIS_MODULE,	//用于指定该设备文件操作函数集合所属的模块。THIS_MODULE 是一个宏，表示当前模块自身
+	.write=write_controlinfo,//用于指定写操作的函数指针。write_controlinfo 是一个自定义的函数，用于处理设备文件的写操作。
 };
 
-
+//模块的初始化函数 initmodule，在加载模块时会被调用。
 static int __init initmodule(void)
 {
 	int ret;
-   printk("Init Module\n");
-   myhook.hook=hook_func;
-   myhook.hooknum=NF_INET_POST_ROUTING;
-   myhook.pf=PF_INET;
-   myhook.priority=NF_IP_PRI_FIRST;
+   printk("Init Module\n");	//内核日志输出初始化信息
+   myhook.hook=hook_func;	//指定网络钩子函数
+   myhook.hooknum=NF_INET_POST_ROUTING;	//指定钩子函数的钩子点，NF_INET_POST_ROUTING 表示在 IP 协议栈的后期处理阶段触发钩子函数。
+   myhook.pf=PF_INET;	//指定协议族，PF_INET 表示 IPv4 协议族
+   myhook.priority=NF_IP_PRI_FIRST;	//指定钩子函数的优先级，NF_IP_PRI_FIRST 表示最高优先级。
 
-   nf_register_net_hook(&init_net,&myhook);
+   nf_register_net_hook(&init_net,&myhook);	//向网络协议栈注册钩子函数。&init_net 表示网络命名空间，&myhook 是指向钩子函数结构体的指针。
 
-   ret = register_chrdev(124, "/dev/controlinfo", &fops); 	// ��ϵͳע���豸����ļ�
-   if (ret != 0) printk("Can't register device file! \n");
+   ret = register_chrdev(124, "/dev/controlinfo", &fops); 	// 向系统注册设备结点文件；
+   																						//向系统注册设备节点文件。124 是设备号，"/dev/controlinfo" 是设备文件路径，
+																						//&fops 是设备文件的操作函数集合。
+   if (ret != 0) printk("Can't register device file! \n");	//注册失败
 
    return 0;
 }
 
+//卸载模块时会被调用
 static void __exit cleanupmodule(void)
 {
 	nf_unregister_net_hook(&init_net,&myhook);
 
-	unregister_chrdev(124, "controlinfo");	 // ��ϵͳע���豸����ļ�
+	unregister_chrdev(124, "controlinfo");	 // 向系统注销设备结点文件
         printk("CleanUp\n");
 }
 
 module_init(initmodule);
 module_exit(cleanupmodule);
 MODULE_LICENSE("GPL");
+
